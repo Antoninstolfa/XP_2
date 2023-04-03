2bd6e68f92f27d9f076e2446640045ad059f7e84
Antoninstolfa <97514759+Antoninstolfa@users.noreply.github.com>
2023-04-03 20:03:49 +0200

    Add files via upload

---
+++ XP_2/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodNeg()
+        {
+            string jmeno = XP_2.Form1.VratJmeno("Tonda");
+            Assert.IsNull(jmeno);
+        }
+        [TestMethod]
+        public void TestMethodPos()
+        {
+            string jmeno = XP_2.Form1.VratJmeno("Tonda");
+            Assert.IsNotNull(jmeno);
+        }
+        [TestMethod]
+        public void TestMethodNeg1()
+        {
+            string jmeno = XP_2.Form1.VratJmeno("Tonda");
+            Assert.IsTrue(jmeno.Length > 10);
+        }
+        [TestMethod]
+        public void TestMethodPos1()
+        {
+            string jmeno = XP_2.Form1.VratJmeno("Tonda") + "2454asd";
+            Assert.IsTrue(jmeno.Length > 10);
+        }
+        [TestMethod]
+        public void TestMethodNeg2()
+        {
+            string jmeno = XP_2.Form1.VratJmeno("Tonda");
+            Assert.IsTrue(jmeno.Contains("&"));
+        }
+        [TestMethod]
+        public void TestMethodPos2()
+        {
+            string jmeno = XP_2.Form1.VratJmeno("Tonda");
+            Assert.IsTrue(jmeno.Contains("Tonda"));
+        }
+        [TestMethod]
+        public void TestMethodNeg3()
+        {
+            XP_2.Form1 form = new XP_2.Form1();
+            string specializace = XP_2.Form1.VratSpecializaci("Berserker");
+            Assert.AreSame(specializace, "Kouzelník");
+        }
+        [TestMethod]
+        public void TestMethodPos3()
+        {
+            string specializace = XP_2.Form1.VratSpecializaci("Berserker");
+            Assert.AreSame(specializace, "Berserker");
+        }
+        [TestMethod]
+        public void TestMethodNeg4()
+        {
+            double aktualni1 = XP_2.Form1.VratXP(20);
+            int aktualni2 = XP_2.Form1.VratXP(20);
+            Assert.AreSame(aktualni1, aktualni2);
+        }
+        [TestMethod]
+        public void TestMethodPos4()
+        {
+            int aktualni = XP_2.Form1.VratXP(20);
+            Assert.AreEqual(aktualni, XP_2.Form1.VratXP(20));
+        }
+        [TestMethod]
+        public void TestMethodNeg5()
+        {
+            int X = XP_2.Form1.ZmenaPoziceX(2);
+            int Y = XP_2.Form1.ZmenaPoziceY(5);
+            Assert.IsFalse(X < Y);
+        }
+        [TestMethod]
+        public void TestMethodPos5()
+        {
+            int X = XP_2.Form1.ZmenaPoziceX(2);
+            int Y = XP_2.Form1.ZmenaPoziceY(5);
+            Assert.IsFalse(X > Y);
+        }
+    }
+}
---
+++ XP_2/XP_2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XP_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string Specializace = "Berserker";
+        public int XP = 20;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public static string VratJmeno(string jmeno)
+        {
+            return jmeno;
+        }
+        public static string VratSpecializaci(string Specializace)
+        {
+            return Specializace;
+        }
+        public static int VratXP(int XP)
+        {
+            return XP;
+        }
+        public static int ZmenaPoziceX(int X)
+        {
+            return X;
+        }
+        public static int ZmenaPoziceY(int Y)
+        {
+            return Y;
+        }
+    }
+}
